b8704b233b6b58764ad3eee53b48bd9285c41f23
sjdok9612 <56342332+sjdok9612@users.noreply.github.com>
2021-05-23 05:14:56 +0900

    Update s.c

--- s.c
+++ s.c
@@ -16,14 +16,22 @@
 
 void error_handling(char* mse);
 void* handle_clnt(void* arg);
+void* status_board(void* arg);
 void send_msg(char* msg, int len);
 
 int turn[4]; 
+
 int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
-int msgQ[5][BUF_SIZE];
+struct Clnt{
+	char IP[16];
+	int PORT;
+};
+struct Clnt C[MAX_CLNT];
+char msgQ[5][BUF_SIZE];
 pthread_mutex_t mutx;
 pthread_t t_id;
+pthread_t t_id2;
 
 int main(int argc, char* argv[])
 {
@@ -48,21 +56,22 @@ int main(int argc, char* argv[])
 	if (listen(serv_sock, 5) == -1) {
 		error_handling("listen() error");
 	}
-	printf("var reset, ip port allocate, listen");
+	printf("var reset, ip port allocate, listen, setting all cleared. start in 5sec \n");
+	sleep(5);
+	pthread_create(&t_id2, NULL, status_board, (void*)&clnt_sock);
+	pthread_detach(t_id2);
 	while (1) {
 		clnt_adr_sz = sizeof(clnt_adr);
 		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
 
 		pthread_mutex_lock(&mutx);
-		clnt_socks[clnt_cnt++] = clnt_sock;
+		clnt_socks[clnt_cnt] = clnt_sock;
 		pthread_mutex_unlock(&mutx);
 
 		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
 		pthread_detach(t_id);
-		printf("Connected Clnt IP : %s \n", inet_ntoa(clnt_adr.sin_addr));
-
-		printf("* %s:%d connect\n", inet_ntoa(clnt_adr.sin_addr), ntohs(clnt_adr.sin_port));
-		printf(".\n");
+		strcpy(C[clnt_cnt].IP,inet_ntoa(clnt_adr.sin_addr));
+		C[clnt_cnt++].PORT=ntohs(clnt_adr.sin_port);
 	}
 	close(serv_sock);
 	return 0;
@@ -80,7 +89,15 @@ void* handle_clnt(void* arg) {
 	int str_len = 0, i;
 	char msg[BUF_SIZE];
 	//handle_clnt의 메세지 수신부분
-	
+	while ((str_len = read(clnt_sock, msg, sizeof(msg))) != 0){
+		printf("[Debug]red\n");
+		strcpy(msgQ[4],msgQ[3]);
+		strcpy(msgQ[3],msgQ[2]);
+		strcpy(msgQ[2],msgQ[1]);
+		strcpy(msgQ[1],msgQ[0]);
+		strcpy(msgQ[0],msg);
+		//send_msg(msg, str_len);
+	}
 	pthread_mutex_lock(&mutx);
 	for (i = 0; i < clnt_cnt; i++) //eliminated disconnections
 	{
@@ -88,6 +105,7 @@ void* handle_clnt(void* arg) {
 		{
 			while (i++ < clnt_cnt - 1)
 				clnt_socks[i] = clnt_socks[i + 1];
+				memcpy(&C[i],&C[i+1],sizeof(struct Clnt));
 			break;
 		}
 	}
@@ -95,6 +113,27 @@ void* handle_clnt(void* arg) {
 	pthread_mutex_unlock(&mutx);
 	close(clnt_sock);
 	return NULL;
+}
+void* status_board(void* arg){
+	while(1){
+		system("clear");
+	//접속클라이언트 현황
+		printf("CLNT\t|IP\t\t|PORT\t|\n");
+		for(int i=0; i<clnt_cnt;i++){
+		printf("%d\t|%s\t|%d\t|\n",i,C[i].IP,C[i].PORT);
+		}
+	//채팅현황
+		printf("\n================================\n");
+		printf("5 recnet msgs\n");
+		for(int i=0; i<5;i++){
+		printf("%d:%s\n",i,msgQ[i]);
+		}
+	//게임 현황
+		printf("\n================================\n");
+		//for(){]
+	//딜레이
+		sleep(2);
+	}
 }
 void send_msg(char* msg, int len) {
 	int i;
